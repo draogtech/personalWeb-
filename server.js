6734f4471b01507c99bdb67fab30ab7c4467174f
draogtech <draogtech@gmail.com>
2018-05-02 08:03:06 +0000

    Changed static port to 3000

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var port = process.env.PORT || 8000;
+var port = process.env.PORT || 3000;
 
 // flashing message to users after sending message on contact form
 var session = require('express-session');
